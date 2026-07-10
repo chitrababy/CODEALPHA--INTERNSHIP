ad5fe6f478ecda984012e56bd7e601c37db1c6eb
R.CHITRA BABY <ramanchitrababy@gmail.com>
2026-07-10 17:09:15 +0530

    Add calculator . c

---
+++ calculator.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+
+int main() 
+{
+    float num1, num2, result;
+    char op;
+    
+    printf("=== BASIC CALCULATOR ===\n");
+    printf("Choose Operation: +  -  *  /\n\n");
+    
+    printf("Enter first number: ");
+    scanf("%f", &num1);
+    
+    printf("Enter operator: ");
+    scanf(" %c", &op);  // space mukkiyam
+    
+    printf("Enter second number: ");
+    scanf("%f", &num2);
+    
+    // Switch case use panniruken
+    switch(op)
+    {
+        case '+':
+            result = num1 + num2;
+            printf("\nResult: %.2f + %.2f = %.2f", num1, num2, result);
+            break;
+            
+        case '-':
+            result = num1 - num2;
+            printf("\nResult: %.2f - %.2f = %.2f", num1, num2, result);
+            break;
+            
+        case '*':
+            result = num1 * num2;
+            printf("\nResult: %.2f * %.2f = %.2f", num1, num2, result);
+            break; 
+            
+        case '/':
+            if(num2 != 0)
+            {
+                result = num1 / num2;
+                printf("\nResult: %.2f / %.2f = %.2f", num1, num2, result);
+            }
+            else
+            {
+                printf("\nError! Division by zero not allowed.");
+            }
+            break;
+            
+        default:
+            printf("\nInvalid operator! Please enter +, -, * or /");
+    }
+    
+    return 0;
+}
